6093f17ed5c7803a0add1ff42c15f432ea6ab640
Hemant Gupta <rajni.hemant@gmail.com>
2013-08-02 12:41:41 +0530

    xA

---
+++ CXF-JAX-WS-2/ReadMe.txt
@@ -0,0 +1,5 @@
+1. Add tomcat 6 in eclipse
+2. Run project in tomcat
+3. Run client
+4. WDSL is at : http://localhost:8080/OrderProcess?WSDL
+5. 
---
+++ CXF-JAX-WS-2/src/demo/order/OrderProcessImpl.java
@@ -0,0 +1,32 @@
+package demo.order; 
+
+import javax.jws.WebService;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@WebService
+@Path("/OrderProcess")
+public class OrderProcessImpl implements OrderProcess {
+	
+	@GET
+	@Path("/op")
+    public String processOrder(Order order) {
+		String orderID = validate(order);
+        return orderID;
+    }
+
+	/**
+	 * Validates the order and returns the order ID
+	**/
+	private String validate(Order order) {
+		String custID = order.getCustomerID();
+		String itemID = order.getItemID();
+		int qty = order.getQty();
+		double price = order.getPrice();
+
+		if (custID != null && itemID != null && !custID.equals("") && !itemID.equals("") && qty > 0 && price > 0.0) {
+			return "ORD1234";
+		}
+		return null;
+	}
+}
